2c8237066ff90694a097c7ab305c0cc357d4d49c
SRIKANTH GUMMA <143108748+srikanth099@users.noreply.github.com>
2024-10-20 21:51:33 +0530

    Add files via upload
    developing calculator operations in java applications

---
+++ calculator/src/main/java/org/SKtechnologies/calculator/App.java
@@ -0,0 +1,14 @@
+package org.SKtechnologies.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(CommonIn.add(30, 40));
+    }
+}
---
+++ calculator/src/main/java/org/SKtechnologies/calculator/CommonIn.java
@@ -0,0 +1,11 @@
+package org.SKtechnologies.calculator;
+
+public class CommonIn {
+	public static int add(int i,int j) {
+		if(i<0 || j<0) {
+			return 0;
+		}
+		return i+j;
+	}
+
+}
